66b1db574a95cf15b509f1189b0155aaef62b884
huymaster <huymastertx88@gmail.com>
2026-07-08 23:09:56 +0700

    feat(permission): add dependency support to PermissionType
    Refs: Default

--- app/src/main/java/com/github/huymaster/materialweather/feature/permission/domain/model/PermissionType.kt
+++ app/src/main/java/com/github/huymaster/materialweather/feature/permission/domain/model/PermissionType.kt
@@ -2,14 +2,17 @@ package com.github.huymaster.materialweather.feature.permission.domain.model
 
 sealed class PermissionType {
     abstract val info: PermissionInfo
-    open val minSdk: Int get() = 0
-    open val maxSdk: Int get() = Int.MAX_VALUE
+    open val minSdk: Int = 0
+    open val maxSdk: Int = Int.MAX_VALUE
+    open val dependencies: List<PermissionType> = emptyList()
+
     val supportedSdk: IntRange get() = minSdk..maxSdk
 
     data class Runtime(
         val permission: String,
         override val info: PermissionInfo,
         override val minSdk: Int,
-        override val maxSdk: Int
+        override val maxSdk: Int,
+        override val dependencies: List<PermissionType> = emptyList()
     ) : PermissionType()
 }
