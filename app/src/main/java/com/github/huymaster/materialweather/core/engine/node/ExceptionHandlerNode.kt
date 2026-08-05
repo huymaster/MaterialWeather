5f9cb541e12d3deb8d5371dbd7dfbce9cba89a9a
huymaster <huymastertx88@gmail.com>
2026-08-05 18:15:24 +0700

    feat(engine): implement ExceptionHandlerNode logic
    - add localized exception messages
    - implement node execution for exception handling
    
    Refs: Default

--- app/src/main/java/com/github/huymaster/materialweather/core/engine/node/ExceptionHandlerNode.kt
+++ app/src/main/java/com/github/huymaster/materialweather/core/engine/node/ExceptionHandlerNode.kt
@@ -1,6 +1,9 @@
 package com.github.huymaster.materialweather.core.engine.node
 
+import android.content.res.Resources
+import com.github.huymaster.materialweather.R
 import com.github.huymaster.materialweather.core.engine.Node
+import com.github.huymaster.materialweather.core.engine.NodeException
 import com.github.huymaster.materialweather.core.engine.NodeExecutionEngine
 import com.github.huymaster.materialweather.core.engine.NodeParam
 import com.github.huymaster.materialweather.core.engine.serialization.RestoreData
@@ -9,16 +12,59 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 class ExceptionHandlerNode(data: RestoreData = RestoreData.EMPTY) : Node() {
-    override val name: Int = 0
+    override val name: Int = R.string.node_exception_handler
 
+    private val exception =
+        NodeParam.input<NodeException>(EXCEPTION_KEY, id = data.getParamId(EXCEPTION_KEY))
     private val _message = MutableSharedFlow<String>(0)
+
     val message: SharedFlow<String> get() = _message.asSharedFlow()
 
-    override fun getInputs(): Set<NodeParam.Input<*>> = setOf()
+    override fun getInputs(): Set<NodeParam.Input<*>> = setOf(exception)
     override fun getOutputs(): Set<NodeParam.Output<*>> = setOf()
 
     override suspend fun execute(context: NodeExecutionEngine.ExecutionContext) {
-        TODO()
+        val exception = context.get<NodeException>(this.exception) ?: skip()
+        val res: Resources = context.androidContext.resources
+        val message = when (exception) {
+            is NodeException.CannotDeserialize ->
+                res.getString(R.string.exception_cannot_deserialize, exception.msg)
+
+            is NodeException.CannotSerializeNode ->
+                res.getString(R.string.exception_cannot_serialize_node, exception.nodeId)
+
+            is NodeException.DuplicateConnection ->
+                res.getString(R.string.exception_duplicate_connection, exception.connectionId)
+
+            is NodeException.FlowSkipped ->
+                res.getString(R.string.exception_flow_skipped, exception.nodeId)
+
+            is NodeException.InfinityLoop ->
+                res.getString(R.string.exception_infinity_loop)
+
+            is NodeException.InvalidFactory ->
+                res.getString(R.string.exception_invalid_factory, exception.node)
+
+            is NodeException.InvalidType ->
+                res.getString(
+                    R.string.exception_invalid_type,
+                    exception.expected.java.name,
+                    exception.actual.java.name
+                )
+
+            is NodeException.MissingRequiredInput ->
+                res.getString(R.string.exception_missing_input, exception.nodeId, exception.paramId)
+
+            is NodeException.NodeFactoryNotFound ->
+                res.getString(R.string.exception_node_factory_not_found, exception.type)
+
+            is NodeException.NodeFactoryRegisteredTwice ->
+                res.getString(R.string.exception_node_factory_registered_twice, exception.types)
+
+            is NodeException.ParamNotFound ->
+                res.getString(R.string.exception_null_param, exception.paramId)
+        }
+        _message.emit(message)
     }
 
     companion object {
